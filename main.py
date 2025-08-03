6018274f6c0ca87e019d496974049efdc1c9ee88
worknech <worknech@yandex.ru>
2025-08-03 11:42:26 +0300

    Добавлено округление координат

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ def get_coordinates(city, key):
         results = geocoder.geocode(city, language='ru')
 
         if results:
-            return results[0]['geometry']['lat'], results[0]['geometry']['lng']
+            lat = round(results[0]['geometry']['lat'], 2)
+            lng = round(results[0]['geometry']['lng'], 2)
+            return lat, lng
         else:
             return "Город не найден"
     except Exception as e:
@@ -15,6 +17,6 @@ def get_coordinates(city, key):
 
 
 key = 'c3ecd8694f8240e5a339af13f133a19b'
-city = 'London'
+city = 'Химки'
 coordinates = get_coordinates(city, key)
 print(f"Координаты города {city}: {coordinates}")
