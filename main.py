2987668346762ff029ff30931b64cc9b5f63b84b
worknech <worknech@yandex.ru>
2025-08-03 13:40:11 +0300

    Добавлена возможность отображения города на карте в браузере

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from unittest import result
-
 from opencage.geocoder import OpenCageGeocode
 from tkinter import *
+import webbrowser
 
 
 def get_coordinates(city, key):
+    global map_url
     try:
         geocoder = OpenCageGeocode(key)
         results = geocoder.geocode(city, language='ru')
@@ -13,11 +13,19 @@ def get_coordinates(city, key):
             lat = round(results[0]['geometry']['lat'], 2)
             lng = round(results[0]['geometry']['lng'], 2)
             country = results[0]['components']['country']
+            osm_url = f"https://www.openstreetmap.org/?mlat={lat}&mlon={lng}"
+
             if 'state' in results[0]['components']:
                 region = results[0]['components']['state']
-                return f"Широта: {lat}, Долгота: {lng},\n Страна: {country},\n Регион: {region}"
+                return {
+                    "coordinates": f"Широта: {lat}, Долгота: {lng},\n Страна: {country},\n Регион: {region}",
+                    "map_url": osm_url
+                        }
             else:
-                return f"Широта: {lat}, Долгота: {lng},\n Страна: {country}"
+                return {
+                    "coordinates": f"Широта: {lat}, Долгота: {lng},\n Страна: {country}",
+                    "map_url": osm_url
+                        }
 
         else:
             return "Город не найден"
@@ -26,16 +34,24 @@ def get_coordinates(city, key):
 
 
 def show_coordinates(event=None):
+    global map_url
     city = entry.get()
-    coordinates = get_coordinates(city, key)
-    label.config(text=f"Координаты города {city}:\n {coordinates}")
+    result= get_coordinates(city, key)
+    label.config(text=f"Координаты города {city}:\n {result['coordinates']}")
+    map_url = result['map_url']
+
+
+def show_map():
+    if map_url:
+        webbrowser.open(map_url)
 
 
 key = 'c3ecd8694f8240e5a339af13f133a19b'
+map_url = None
 
 root = Tk()
 root.title("Координаты городов")
-root.geometry("350x120")
+root.geometry("350x160")
 
 entry = Entry(root)
 entry.pack()
@@ -46,5 +62,8 @@ button.pack()
 
 label = Label(root, text="Введите город и нажмите на кнопку")
 label.pack()
+
+map_button = Button(text="Показать карту", command=show_map)
+map_button.pack()
 
 root.mainloop()
